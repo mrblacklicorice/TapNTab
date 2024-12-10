989c8d72b9e8c136f772492351f7d9bf0a6df31a
NicoleRobak <nikki.robak@gmail.com>
2024-12-10 16:03:17 -0600

    Restored MainActivity.kt from commit 9a545d2

--- app/src/main/java/com/cs407/tapntab/MainActivity.kt
+++ app/src/main/java/com/cs407/tapntab/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.cs407.tapntab
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Navigate to LoginActivity when loginButton is clicked
+        findViewById<View>(R.id.loginButtonFirstScreen).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Navigate to SignupActivity when signUpButton is clicked
+        findViewById<View>(R.id.signUpButton).setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
         }
     }
 }
